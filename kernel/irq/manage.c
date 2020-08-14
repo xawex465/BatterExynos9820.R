9d39227912329e2a2df200654412c48f08161c7e
Sultan Alsawaf <sultan@kerneltoast.com>
2020-08-14 21:09:13 +0200

    kernel: Warn when an IRQ's affinity notifier gets overwritten

--- kernel/irq/manage.c
+++ kernel/irq/manage.c
@@ -329,6 +329,8 @@ irq_set_affinity_notifier(unsigned int irq, struct irq_affinity_notify *notify)
 	raw_spin_unlock_irqrestore(&desc->lock, flags);
 
 	if (old_notify)
+                if (notify)
+			WARN(1, "overwriting previous IRQ affinity notifier\n");
 		kref_put(&old_notify->kref, old_notify->release);
 
 	return 0;
